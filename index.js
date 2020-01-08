493bd579369ad93eb7e45cce9daf60a13d50b633
Chengyu Ko <kokevin678@gmail.com>
2020-01-07 23:03:03 -0800

    fixed prompt user for input

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const electron = require("electron");
+const convertFactory = require("electron-html-to");
 const axios = require("axios");
+const generateHTML = require("./generateHTML");
 
 const questions = [
     {
         type: "input",
-        name: "GitHub",
+        name: "username",
         message: "Enter GitHub username",
     },
     {
@@ -20,8 +22,21 @@ const questions = [
 
 function writeToFile(fileName, data) {
  
-}
+};
 
 function init() {
+    inquirer.prompt(questions).then(function({username, color}) {
+        const queryURL = "https://api.github.com/users/${username}";
+        console.log(username, color);
 
+        axios.get(queryURL).then(function(response) {
+            const name = response.data.name;
+            const location = response.data.location;
+            const githubLink = response.data.html_url;
+            const publicRepos = response.data.public_repos;
+            const followers = response.data.followers;
+            const following = response.data.following; 
+        });
+    });
+}
 init();
