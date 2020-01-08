0e18d6987fe159d0d6e446d45a353ee3365dffcd
Chengyu Ko <kokevin678@gmail.com>
2020-01-07 19:07:14 -0800

    add github UN and color question

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 const questions = [
+    {
+        type: "input",
+        name: "GitHub",
+        message: "Enter GitHub username",
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "Favorite color?",
+        choices: ["Green", "Blue", "Pink", "Red"],
+    }
   
 ];
 
